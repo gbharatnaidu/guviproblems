5462f81930f9fccedd8baa878f8e46fa70137f8c
gbharatnaidu <38840613+gbharatnaidu@users.noreply.github.com>
2019-06-18 14:41:33 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main(){
-int n,sum=0;
+int n;
 scanf("%d",&n);
-for(int i=1;i<=n;i++){
-    sum=sum+i;
-}
-printf("%d",sum);
-return 0;
+    for(int i=1;i<=n;i++){
+        printf("Hello\n");
+    }
+        return 0;
 }
