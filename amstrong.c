a71a700473e3970874c95cdadc1c69bc3472a8ce
gbharatnaidu <38840613+gbharatnaidu@users.noreply.github.com>
2019-07-05 23:05:29 +0530

    Create amstrong.c

---
+++ amstrong.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+int n,c=0,sum=0,rem;
+scanf("%d",n);
+int k=n,m=n;
+while(n>0){
+    c++;
+    n=n/10;
+}
+while(k>0){
+    rem=k%10;
+    sum=sum+pow(rem,c);
+    k=k/10;
+}
+if(sum==m)
+    printf("yes");
+else
+    printf("no");
+return 0;
+}
