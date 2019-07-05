fc85dab416ca3282fb8f837d410cdc79efcd7891
gbharatnaidu <38840613+gbharatnaidu@users.noreply.github.com>
2019-07-05 23:33:51 +0530

    Update fib.c

--- fib.c
+++ fib.c
@@ -6,6 +6,8 @@ if(n==1)
 printf("%d ",a);
 if(n==2)
 printf("%d %d ",a,b);
+if(n>2)
+printf("%d %d ",a,b);
 for(i=1;i<=n-2;i++){
     c=a+b;
     a=b;
@@ -14,4 +16,3 @@ for(i=1;i<=n-2;i++){
 }
 return 0;
 }
-
