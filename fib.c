796507c25c4b69ad9f73e899cd5a6c8349c9460a
gbharatnaidu <38840613+gbharatnaidu@users.noreply.github.com>
2019-07-05 23:30:51 +0530

    Update fib.c

--- fib.c
+++ fib.c
@@ -2,6 +2,9 @@
 int main(){
 int n,a=1,b=1,c,i;
 scanf("%d",&n);
+if(n==1)
+printf("%d ",a);
+if(n==2)
 printf("%d %d ",a,b);
 for(i=1;i<=n-2;i++){
     c=a+b;
@@ -11,3 +14,4 @@ for(i=1;i<=n-2;i++){
 }
 return 0;
 }
+
