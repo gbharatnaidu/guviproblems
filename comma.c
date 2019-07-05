8ce090a1077e89bdeb26954d13e00a016b35c7a8
gbharatnaidu <38840613+gbharatnaidu@users.noreply.github.com>
2019-07-06 00:18:55 +0530

    Create comma.c

---
+++ comma.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+char str[100];
+int count=0,i;
+gets(str);
+for(i=0;str[i]!='\0';i++){
+    if(str[i]=='.')
+        count++;
+}
+printf("%d",count+1);
+return 0;
+}
