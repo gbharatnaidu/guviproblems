0c109985ff7d085666a3387b58c66fc5363d9da2
gbharatnaidu <38840613+gbharatnaidu@users.noreply.github.com>
2019-07-05 23:51:22 +0530

    Update spc.c

--- spc.c
+++ spc.c
@@ -4,7 +4,7 @@ char str[100];
 int count=0,i;
 gets(str);
 for(i=0;str[i]!='\0';i++){
-    if(!((str[i]>=97 && str[i]<=122) || (str[i]>=0 && str[i]<=9)))
+    if(!((str[i]>=97 && str[i]<=122) || (str[i]>=0 && str[i]<=9) || (str[i]==' ')))
         count++;
 }
 printf("%d",count);
