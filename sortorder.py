f34c32f072031fe1bf3ba379cbf41ed852c8a79b
Gutha Bharath Kumar <38840613+gbharatnaidu@users.noreply.github.com>
2019-07-16 13:15:47 +0530

    Update sortorder.py

--- sortorder.py
+++ sortorder.py
@@ -1,11 +1,15 @@
 n=int(input())
 lst=input()
+k=0
 emp=[]
 lst=lst.split()
 lst=list(map(int,lst))
 for i in range(0,n):
  if lst[i]==i:
    emp.append(i)
+   k=1
 for i in range(0,len(emp)):
   print(min(emp),end=" ")
   emp.remove(min(emp))
+  if(k==0):
+    print("-1")
