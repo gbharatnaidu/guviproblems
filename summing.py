a18a2fa83d32f5c1b7cb4da52cc60a0de3129593
Gutha Bharath Kumar <38840613+gbharatnaidu@users.noreply.github.com>
2019-07-16 13:58:44 +0530

    Update summing.py

--- summing.py
+++ summing.py
@@ -6,4 +6,4 @@ for i in range(0,n):
   for j in range(i+1,n):
     for k in range(j+1,n):
       if((lst[i]+lst[j])==lst[k]):
-        print(lst[i]," ",lst[j]," ",lst[k])
+        print(lst[i],lst[j],lst[k])
