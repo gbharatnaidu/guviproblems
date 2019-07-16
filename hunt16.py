1d881223f7fd89c4a4c2ae0475ee9f3d6175fda3
Gutha Bharath Kumar <38840613+gbharatnaidu@users.noreply.github.com>
2019-07-16 13:28:27 +0530

    Update hunt16.py

--- hunt16.py
+++ hunt16.py
@@ -1,8 +1,12 @@
 n=int(input())
 lst=input()
+k=0
 lst=lst.split()
 lst=list(map(int,lst))
 for i in range(0,n):
  if(lst.count(lst[i])==2):
    print(lst[i])
+   k=1
    break
+if(k==0):
+ print("unique")
